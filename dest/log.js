bd54cbbebbe7ace6014d419753161f48857e43a2
davidbuzatu-marian <davidm.buzatu@gmail.com>
2024-05-31 18:03:50 +0200

    Fixed compilation error caused by missing es2022

--- dest/log.js
+++ dest/log.js
@@ -1,14 +1,17 @@
 import * as fs from "fs";
-const log = fs.createWriteStream("/tmp/lsp1.log");
+const log = fs.createWriteStream("/tmp/jsoniq-lsp.log");
 export default {
     write: (message) => {
-        if (typeof message === "object") {
-            log.write(JSON.stringify(message));
+        // Only write if verbosity enabled
+        if (!!process.env.VERBOSE) {
+            if (typeof message === "object") {
+                log.write(JSON.stringify(message));
+            }
+            else {
+                log.write(message);
+            }
+            log.write("\n");
         }
-        else {
-            log.write(message);
-        }
-        log.write("\n");
     },
 };
 //# sourceMappingURL=log.js.map
